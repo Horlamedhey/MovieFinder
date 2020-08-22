e7de0193d51dfdb06e4eeaa226f3b0bf9573df87
AbdulGafar Olamide Ajao <gafarolamide.ajao@gmail.com>
2020-08-22 11:31:28 +0100

    -Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#B80B69',
+        gray: {
+          '100': '#f5f5f5',
+          '200': '#eeeeee',
+          '300': '#e0e0e0',
+          '400': '#bdbdbd',
+          '500': '#9e9e9e',
+          '600': '#757575',
+          '700': '#616161',
+          '800': '#424242',
+          '900': '#212121',
+        },
+        'gray-variant': '#969393',
+        'gray-variant1': '#878585',
+        'gray-variant2': '#F4F4F4',
+        'black-variant': 'rgba(0,0,0,0.40)',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+}
